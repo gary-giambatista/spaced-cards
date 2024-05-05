6540a937d8ec77823285d6c79a833b273fbdf27d
Gary Giambatista <gary.giambatista@gmail.com>
2024-05-05 13:58:09 -0400

    Added logo and replaced favicon

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,12 @@
+import Logo from "@/public/Logo.png";
+import Image from "next/image";
 import React from "react";
 
 function Header() {
 	return (
 		<header className="min-h-16 bg-slate-800 flex justify-between items-center px-4">
-			<div className="flex gap-x-2 ">
-				<div>Image Placeholder</div>
+			<div className="flex justify-center items-center gap-x-2 ">
+				<Image height={60} width={60} src={Logo} alt="Logo Image" />
 				<h2>Spaced Cards</h2>
 			</div>
 			{/* Username button Component Clerk here  */}
