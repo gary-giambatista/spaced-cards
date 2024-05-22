dc85d9513926bfb9be2c2bff12607ce863796ce0
Gary Giambatista <gary.giambatista@gmail.com>
2024-05-21 22:08:12 -0400

    Implemented 3 color repeating background via Card_Rows.jsx and Mini_Card.jsx. Initial design for Mini cards need to polish and figure it out further.

--- components/hud/Overview/Card_Rows.jsx
+++ components/hud/Overview/Card_Rows.jsx
@@ -2,18 +2,20 @@ import React from "react";
 import Mini_Card from "./Mini_Card";
 
 function Card_Rows({ selectedDeck, setSelectedCardId }) {
-	//TODO: Implement Edit Card Modal and functionality (and delete)
-	// 1. Create state for selectedCard / or use Mini_Card instead
-	// 2. Create a modal similar to Add_Card_Modal to edit -- how to handle the tree / display absolute issue
-	// 3. Pass selectedCard's info to Edit_Card_Modal
-	// 4. Render selectedCard's info in Edit_Card_Modal
-	// 5. Write a function similar to practice() to update that specific card in selectedDeck
+	// const colors = ["bg-red-500", "bg-green-500", "bg-blue-500"];
+	const colors = ["bg-[#5FB55D]", "bg-[#CBD748]", "bg-[#D74848]"];
+
 	return (
 		<div className="grid grid-cols-1 @md:grid-cols-2 @2xl:grid-cols-3 @3xl:grid-cols-4 @5xl:grid-cols-5 @7xl:grid-cols-6 @[1921px]:grid-cols-8 gap-4">
-			{selectedDeck.cards.map((card) => {
+			{selectedDeck.cards.map((card, index) => {
+				const colorClass = colors[index % colors.length];
 				return (
 					<React.Fragment key={card.id}>
-						<Mini_Card card={card} setSelectedCardId={setSelectedCardId} />
+						<Mini_Card
+							card={card}
+							setSelectedCardId={setSelectedCardId}
+							color={colorClass}
+						/>
 					</React.Fragment>
 				);
 			})}
--- components/hud/Overview/Mini_Card.jsx
+++ components/hud/Overview/Mini_Card.jsx
@@ -1,20 +1,22 @@
 import React, { useState } from "react";
 
-function Mini_Card({ card, setSelectedCardId }) {
+function Mini_Card({ card, setSelectedCardId, color }) {
 	// Controls where the card is being edited or not
 	// const [editCardModalIsOpen, setEditCardModalIsOpen] = useState(false);
 
 	return (
 		<div
-			className="flex flex-col h-80 w-full bg-slate-400 hover:opacity-50 hover:cursor-pointer rounded-md p-4"
+			className={`flex flex-col h-80 w-full ${color} hover:opacity-50 hover:cursor-pointer rounded-md p-4  justify-center items-center  `}
 			onClick={() => setSelectedCardId(card.id)}
 		>
-			<div>Question:</div>
-			<div className="pb-4">{card.question}</div>
-			<div>Answer:</div>
-			<div>{card.answer}</div>
+			<div className="font-extrabold">Question:</div>
+			<div className="">{card.question}</div>
+			{/* <div>Answer:</div>
+			<div>{card.answer}</div> */}
 		</div>
 	);
 }
-
+// bg-neutral-100
+// dark:bg-neutral-800 dark:border-neutral-600
+// border-8 border-white shadow-md
 export default Mini_Card;
