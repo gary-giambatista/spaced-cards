25961d53c4d4800e3a1469af4a91cdb9cb86c8ae
Gary Giambatista <gary.giambatista@gmail.com>
2024-05-23 20:30:40 -0400

    Added Edit Icon SVG on Mini_Card.jsx. Styled for desktop and mobile, with nice hover effect and sibling hover effect.

--- components/hud/Overview/Mini_Card.jsx
+++ components/hud/Overview/Mini_Card.jsx
@@ -28,14 +28,15 @@ function Mini_Card({ card, setSelectedCardId, color }) {
 				</div>
 
 				<div
-					className={`[transform:rotateY(180deg)] absolute inset-0 m-auto w-fit h-fit transition-opacity duration-500 font-extrabold ${
-						isFlipped ? "opacity-100" : "opacity-0 "
+					className={` absolute inset-0 m-auto w-fit h-fit [transform:rotateY(180deg)] transition-opacity duration-500 font-extrabold ${
+						isFlipped ? "opacity-100" : "opacity-0"
 					}`}
 				>
 					{card.answer}
 				</div>
+				{/* Flip SVG */}
 				<div
-					className={`absolute inset-0 transition-transform duration-500 [transform:rotateY(180deg)] ${
+					className={`absolute flex justify-between inset-0 transition-transform duration-500 [transform:rotateY(180deg)] ${
 						isFlipped
 							? "[transform:rotateY(180deg)] "
 							: "[transform:revert!important] "
@@ -48,12 +49,26 @@ function Mini_Card({ card, setSelectedCardId, color }) {
 						viewBox="0 0 24 24"
 						strokeWidth={1.5}
 						stroke="currentColor"
-						className="size-6 hover:scale-105"
+						className="size-6 hover:scale-105 peer"
 					>
 						<path
 							strokeLinecap="round"
 							strokeLinejoin="round"
 							d="m15 15 6-6m0 0-6-6m6 6H9a6 6 0 0 0 0 12h3"
+						/>
+					</svg>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						strokeWidth={1.5}
+						stroke="currentColor"
+						className="size-6 opacity-100 lg:opacity-0 transition-opacity duration-500 group-hover:opacity-100 peer-hover:opacity-0"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125"
 						/>
 					</svg>
 				</div>
